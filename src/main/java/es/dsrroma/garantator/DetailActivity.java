b7eb3afb8783f1c155b323fdc02c89d3c0775f6e
Mariona DSR <mariona.nadal@dsrroma.es>
2017-08-09 17:33:13 +0200

    Added edit and delete menus to detail activity.

--- src/main/java/es/dsrroma/garantator/DetailActivity.java
+++ src/main/java/es/dsrroma/garantator/DetailActivity.java
@@ -1,13 +1,20 @@
 package es.dsrroma.garantator;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import es.dsrroma.garantator.data.model.Warranty;
+import es.dsrroma.garantator.data.services.WarrantyUpdateService;
 import es.dsrroma.garantator.utils.CursorToBeanUtils;
+
+import static es.dsrroma.garantator.data.contracts.WarrantyContract.WARRANTY_CONTENT_URI;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -31,5 +38,39 @@ public class DetailActivity extends AppCompatActivity {
             tvWarrantyName.setText(warranty.getName());
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_edit:
+                Toast.makeText(this, "Edit!! " + warranty.getName(), Toast.LENGTH_SHORT).show();
+                editWarranty();
+                return true;
+            case R.id.action_delete:
+                Toast.makeText(this, "Delete!! " + warranty.getId(), Toast.LENGTH_SHORT).show();
+                deleteWarranty();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteWarranty() {
+        Uri uri = WARRANTY_CONTENT_URI.buildUpon().appendPath(warranty.getId() + "").build();
+        WarrantyUpdateService.deleteWarranty(this, uri);
+        finish();
+    }
+
+    private void editWarranty() {
+        Intent intent = new Intent(this, AddWarrantyActivity.class);
+        // TODO pass warranty to edit
+        startActivity(intent);
     }
 }
