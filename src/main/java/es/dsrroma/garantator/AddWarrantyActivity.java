fd508608faf23fb3f9ef475bfa660937e99206de
Mariona DSR <mariona.nadal@dsrroma.es>
2017-08-11 18:46:28 +0200

    Autocomplete now returns id too. New data saved on memory.

--- src/main/java/es/dsrroma/garantator/AddWarrantyActivity.java
+++ src/main/java/es/dsrroma/garantator/AddWarrantyActivity.java
@@ -12,18 +12,23 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.FilterQueryProvider;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-import es.dsrroma.garantator.data.contracts.BrandContract;
-import es.dsrroma.garantator.data.contracts.CategoryContract;
-import es.dsrroma.garantator.data.contracts.WarrantyContract;
+import es.dsrroma.garantator.data.model.Brand;
+import es.dsrroma.garantator.data.model.Category;
+import es.dsrroma.garantator.data.model.Product;
 import es.dsrroma.garantator.data.model.Warranty;
 import es.dsrroma.garantator.data.services.WarrantyUpdateService;
 import es.dsrroma.garantator.utils.CursorToBeanUtils;
 
+import static es.dsrroma.garantator.data.contracts.BaseContract.BaseEntry.COLUMN_ID;
+import static es.dsrroma.garantator.data.contracts.BaseContract.BaseEntry.COLUMN_NAME;
 import static es.dsrroma.garantator.data.contracts.BrandContract.BRAND_CONTENT_URI;
 import static es.dsrroma.garantator.data.contracts.CategoryContract.CATEGORY_CONTENT_URI;
 import static es.dsrroma.garantator.data.contracts.WarrantyContract.WARRANTY_CONTENT_URI;
@@ -41,6 +46,10 @@ public class AddWarrantyActivity extends AppCompatActivity implements
     private boolean editMode;
 
     private Warranty warranty;
+    private Product product;
+    private Brand brand;
+    private Category category;
+
     private Cursor cursor;
 
     private static final int BRAND_LOADER_ID = 2;
@@ -55,17 +64,13 @@ public class AddWarrantyActivity extends AppCompatActivity implements
         actvBrand = (AutoCompleteTextView) findViewById(R.id.actvBrand);
         actvCategory = (AutoCompleteTextView) findViewById(R.id.actvCategory);
 
-        brandAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_dropdown_item_1line, null,
-                new String[] {BrandContract.BrandEntry.COLUMN_NAME},
-                new int[] { android.R.id.text1 }, 0);
-        brandAdapter.setStringConversionColumn(1);
+        brandAdapter = prepareAutocompleteAdapter(BRAND_CONTENT_URI);
         actvBrand.setAdapter(brandAdapter);
+        actvBrand.setThreshold(1);
 
-        categoryAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_dropdown_item_1line, null,
-                new String[] {CategoryContract.CategoryEntry.COLUMN_NAME},
-                new int[] { android.R.id.text1 }, 0);
-        categoryAdapter.setStringConversionColumn(1);
+        categoryAdapter = prepareAutocompleteAdapter(CATEGORY_CONTENT_URI);
         actvCategory.setAdapter(categoryAdapter);
+        actvCategory.setThreshold(1);
 
         final Uri warrantyUri = getIntent().getData();
         editMode = warrantyUri != null;
@@ -74,15 +79,47 @@ public class AddWarrantyActivity extends AppCompatActivity implements
 
             if (cursor.moveToFirst()) {
                 warranty = CursorToBeanUtils.cursorToBean(cursor, 0, Warranty.class);
+                product = warranty.getProduct();
+                brand = product.getBrand();
+                category = product.getCategory();
+
                 etName.setText(warranty.getName());
             }
         } else { // add mode
             warranty = new Warranty();
+            product = new Product();
+            brand = new Brand();
+            category = new Category();
+            warranty.setProduct(product);
+            product.setBrand(brand);
+            product.setCategory(category);
         }
 
         setListeners();
+
         getSupportLoaderManager().initLoader(BRAND_LOADER_ID, null, this);
         getSupportLoaderManager().initLoader(CATEGORY_LOADER_ID, null, this);
+    }
+
+    private SimpleCursorAdapter prepareAutocompleteAdapter(final Uri uri) {
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.dropdown_item_name_id, null,
+                new String[] {COLUMN_NAME, COLUMN_ID},
+                new int[] { R.id.dropdown_name, R.id.dropdown_id}, 0);
+        adapter.setStringConversionColumn(1);
+        adapter.setFilterQueryProvider(new FilterQueryProvider() {
+
+            public Cursor runQuery(CharSequence constraint) {
+                String partialItemName = "";
+                if (constraint != null) {
+                    partialItemName = constraint.toString();
+                }
+
+                return getContentResolver().query(uri, new String[] {},
+                        COLUMN_NAME + " LIKE '%" + partialItemName + "%'",
+                        null, null);
+            }
+        });
+        return adapter;
     }
 
     private void setListeners() {
@@ -100,6 +137,30 @@ public class AddWarrantyActivity extends AppCompatActivity implements
             @Override
             public void afterTextChanged(Editable s) {
                 warranty.setName(s.toString());
+            }
+        });
+
+        actvBrand.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor)parent.getItemAtPosition(position);
+                if (cursor.moveToPosition(position)) {
+                    String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
+                    brand.setName(name);
+                    brand.setId(id);
+                }
+            }
+        });
+
+        actvCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor)parent.getItemAtPosition(position);
+                if (cursor.moveToPosition(position)) {
+                    String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
+                    category.setName(name);
+                    category.setId(id);
+                }
             }
         });
     }
@@ -182,7 +243,7 @@ public class AddWarrantyActivity extends AppCompatActivity implements
 
     private ContentValues warrantyToContentValues() {
         ContentValues cv = new ContentValues();
-        cv.put(WarrantyContract.WarrantyEntry.COLUMN_NAME, warranty.getName());
+        cv.put(COLUMN_NAME, warranty.getName());
         return cv;
     }
 }
